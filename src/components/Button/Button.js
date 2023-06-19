d81944e1e9b95d80649b0e0857844eb9bc42e001
nagul meera <nagul3628@gmail.com>
2023-06-19 15:10:23 +0530

    button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,11 @@
 import './button.scss';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 function Button(props){
     return(
         <button className={`${props.bgColor ?props.bgColor : ""} ${props.btntype ?props.btntype : ""}`}>{props.text}</button>
     )
 }
+
 
 export default Button;
