bb9aea6d5bf66766bcbd170efb4949e4452ec91f
nagul meera <nagul3628@gmail.com>
2023-06-20 14:38:01 +0530

    btn_2_components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter'
+import Button from './components/Button'
+import { useState } from 'react';
+import Mouseover from './components/Mouseover';
 
 function App() {
+const [Count,setCount] = useState(0)
+   
+function handleIncrement(x){
+  setCount(x);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Counter value={Count}/>
+     <Button value={Count} setValue={handleIncrement}/>
+
+
+     <Mouseover />
     </div>
   );
 }
